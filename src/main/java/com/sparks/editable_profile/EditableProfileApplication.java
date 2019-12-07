cbc2e9e43be6766e0eb96a255539d668cfad29a0
Nandak <nandak@nextgen.com>
2019-12-08 05:22:30 +0530

    Commit the main class

---
+++ src/main/java/com/sparks/editable_profile/EditableProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sparks.editable_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditableProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditableProfileApplication.class, args);
+    }
+
+}
