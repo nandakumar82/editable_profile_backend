8eb88fc567fe3282357596a925198d22bc0a35fd
Nandak <nandak@nextgen.com>
2019-12-08 15:12:47 +0530

    Main class test

---
+++ src/test/java/com/sparks/editable_profile/EditableProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparks.editable_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditableProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
