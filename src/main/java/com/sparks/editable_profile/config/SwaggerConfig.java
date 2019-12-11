2d1464fcf4ac4bde6783cdd712ac5928a4ced85a
Nandak <nandak@nextgen.com>
2019-12-11 11:38:23 +0530

    Update pom to include Actuators, enable swagger filter to redirect to swaggerUi by default

--- src/main/java/com/sparks/editable_profile/config/SwaggerConfig.java
+++ src/main/java/com/sparks/editable_profile/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.sparks.editable_profile.config;
 
+import com.sparks.editable_profile.filter.SwaggerFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,11 +18,22 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Autowired
+    private SwaggerFilter myFilter;
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("com.sparks.editable_profile.controllers"))
                 .paths(regex("/api.*"))
                 .build();
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(myFilter);
+        return filterRegistrationBean;
     }
 }
---
+++ src/main/java/com/sparks/editable_profile/filter/SwaggerFilter.java
@@ -0,0 +1,43 @@
+package com.sparks.editable_profile.filter;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Nandak on Dec, 2019
+ */
+@Component
+public class SwaggerFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (request.getRequestURI().equals("") || request.getRequestURI().equals("/")) {
+            response.sendRedirect("/swagger-ui.html");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
